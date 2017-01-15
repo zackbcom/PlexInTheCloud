d0c5576de442228317c565df2230f5079cff5847
chrisanthropic <ctarwater@gmail.com>
2017-01-14 17:33:44 -0800

    refactor couchpotato script

--- 05-couchpotato.sh
+++ 05-couchpotato.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 source vars
 
-# Install CouchPotato
-## Create our Directory
-mkdir /home/$username/nzbget/completed/movies
-chown -R $username:$username /home/$username/nzbget/completed/movies
-mkdir /home/$username/$local/movies
-chown -R $username:$username /home/$username/$local/movies
+## INFO
+# This script installs and configures couchpotato
+##
 
-## Install dependencies
+#######################
+# Dependencies
+#######################
 apt-get install -y git-core libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential
 
 pip install lxml cryptography pyopenssl
 
-## Install Couchpotato
-mkdir /opt/couchpotato
+#######################
+# Install
+#######################
 git clone https://github.com/CouchPotato/CouchPotatoServer.git /opt/couchpotato/
 chown -R $username:$username /opt/couchpotato
 
+#######################
+# Configure
+#######################
 ## Write CouchPotato API to nzbget.conf so it can send post-processing requests
 ### Copy the api key from the CP config file
 cpAPI=$(cat /home/$username/.couchpotato/settings.conf | grep "api_key = ................................" | cut -d= -f 2)
@@ -41,8 +44,29 @@ sed -i "s/^category = Movies/category = movies/g" /home/$username/.couchpotato/s
 perl -i -0pe "s/username = nzbget\ncategory = Movies\ndelete_failed = True\nmanual = 0\nenabled = 0\npriority = 0\nssl = 0/username = $username\ncategory = movies\ndelete_failed = True\nmanual = 0\nenabled = 1\npriority = 0\n ssl = 0/" /home/$username/.couchpotato/settings.conf
 perl -i -0pe "s/6789\npassword =/6789\npassword = $cppasswd\n/" /home/$username/.couchpotato/settings.conf
 
-## POST PROCESSING
-## Add our upload script that uploads Movies to Amazon Cloud Drive
+## Post Processing
+## NZBget
+sed -i "s/^Category1.Name=.*/Category1.Name=movies/g" /opt/nzbget/nzbget.conf
+sed -i "s|^Category1.DestDir=.*|Category1.DestDir=/home/$username/nzbget/completed/movies|g" /opt/nzbget/nzbget.conf
+sed -i "s/^Category1.PostScript=.*/Category1.PostScript=nzbToCouchPotato.py, Logger.py, uploadMovies.sh/g" /opt/nzbget/nzbget.conf
+
+# nzbToCouchPotato
+sed -i 's/^nzbToCouchPotato.py:auto_update=.*/nzbToCouchPotato.py:auto_update=1/g' /opt/nzbget/nzbget.conf
+sed -i 's/^nzbToCouchPotato.py:cpsCategory=.*/nzbToCouchPotato.py:cpsCategory=movies/g' /opt/nzbget/nzbget.conf
+sed -i 's/^nzbToCouchPotato.py:cpsdelete_failed=.*/nzbToCouchPotato.py:cpsdelete_failed=1/g' /opt/nzbget/nzbget.conf
+sed -i 's/^nzbToCouchPotato.py:getSubs=.*/nzbToCouchPotato.py:getSubs=1/g' /opt/nzbget/nzbget.conf
+sed -i "s/^nzbToCouchPotato.py:subLanguages=.*/nzbToCouchPotato.py:subLanguages=$openSubtitlesLang/g" /opt/nzbget/nzbget.conf
+sed -i "s|^nzbToCouchPotato.py:cpswatch_dir=.*|nzbToCouchPotato.py:cpswatch_dir=/home/$username/nzbget/completed/movies|g" /opt/nzbget/nzbget.conf
+
+#######################
+# Structure
+#######################
+mkdir /home/$username/nzbget/completed/movies
+mkdir /home/$username/$local/movies
+
+#######################
+# Helper Scripts
+#######################
 tee "/home/$username/nzbget/scripts/uploadMovies.sh" > /dev/null <<EOF
 #!/bin/bash
 
@@ -64,30 +88,11 @@ rclone move -c /home/$username/$local/movies $encrypted:movies
 exit 93
 EOF
 
-chmod +x /home/$username/nzbget/scripts/uploadMovies.sh
-
-# CATEGORIES
-## Movies
-sed -i "s/^Category1.Name=.*/Category1.Name=movies/g" /opt/nzbget/nzbget.conf
-sed -i "s|^Category1.DestDir=.*|Category1.DestDir=/home/$username/nzbget/completed/movies|g" /opt/nzbget/nzbget.conf
-sed -i "s/^Category1.PostScript=.*/Category1.PostScript=nzbToCouchPotato.py, Logger.py, uploadMovies.sh/g" /opt/nzbget/nzbget.conf
-
-# nzbToCouchPotato
-sed -i 's/^nzbToCouchPotato.py:auto_update=.*/nzbToCouchPotato.py:auto_update=1/g' /opt/nzbget/nzbget.conf
-sed -i 's/^nzbToCouchPotato.py:cpsCategory=.*/nzbToCouchPotato.py:cpsCategory=movies/g' /opt/nzbget/nzbget.conf
-sed -i 's/^nzbToCouchPotato.py:cpsdelete_failed=.*/nzbToCouchPotato.py:cpsdelete_failed=1/g' /opt/nzbget/nzbget.conf
-sed -i 's/^nzbToCouchPotato.py:getSubs=.*/nzbToCouchPotato.py:getSubs=1/g' /opt/nzbget/nzbget.conf
-sed -i "s/^nzbToCouchPotato.py:subLanguages=.*/nzbToCouchPotato.py:subLanguages=$openSubtitlesLang/g" /opt/nzbget/nzbget.conf
-sed -i "s|^nzbToCouchPotato.py:cpswatch_dir=.*|nzbToCouchPotato.py:cpswatch_dir=/home/$username/nzbget/completed/movies|g" /opt/nzbget/nzbget.conf
-
-
-
-## Systemd Service file
+#######################
+# Systemd Service File
+#######################
 cp -v /opt/couchpotato/init/couchpotato.service /etc/systemd/system/couchpotato.service
-chown root:root /etc/systemd/system/couchpotato.service
-chmod 644 /etc/systemd/system/couchpotato.service
 
-## Start couchpotato at boot
 tee "/etc/systemd/system/couchpotato.service" > /dev/null <<EOF
 [Unit]
 Description=CouchPotato application instance
@@ -103,11 +108,26 @@ Group=$username
 WantedBy=multi-user.target
 EOF
 
+#######################
+# Permissions
+#######################
+chown -R $username:$username /home/$username/nzbget/completed/movies
+chown -R $username:$username /home/$username/$local/movies
+chmod +x /home/$username/nzbget/scripts/uploadMovies.sh
+chown root:root /etc/systemd/system/couchpotato.service
+chmod 644 /etc/systemd/system/couchpotato.service
+
+#######################
+# Autostart
+#######################
 systemctl daemon-reload
 systemctl start couchpotato
 systemctl enable couchpotato
 systemctl restart couchpotato
 
+#######################
+# Remote Access
+#######################
 echo ''
 echo "Do you want to allow remote access to CouchPotato?"
 echo "If so, you need to tell UFW to open the port."
@@ -120,4 +140,3 @@ select yn in "Yes" "No"; do
         No ) echo "Port 5050 left closed. You can still access it from your local machine by issuing the following command: ssh $username@$ipaddr -L 5050:localhost:5050"; echo "and then open localhost:5050 on your browser."; exit;;
     esac
 done
-
